21400c5c1492c89031f54bc9e1a634dd11541cea
Landon Thull <98464797+landon-thull@users.noreply.github.com>
2022-06-30 14:38:14 -0500

    ui structure

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./Sidebar.scss";
+
+import { P2p } from "../../svg.js";
+
+class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="SideBar">
+        <button className="side-button">Peer to Peer</button>
+        <button className="side-button">Forum</button>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
---
+++ src/svg.js
@@ -0,0 +1,10 @@
+const P2p = () => {
+  return (
+    <svg viewBox="24 24 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M7.75732 7.75732L16.2426 16.2426" />
+      <path d="M7.75739 16.2426L16.2427 7.75732" />
+    </svg>
+  );
+};
+
+export { P2p };
