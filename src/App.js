8f6896b260c43798430f06ffdb23e028d182217d
Landon Thull <98464797+landon-thull@users.noreply.github.com>
2022-07-04 18:40:53 -0500

    fixed switchNet

--- src/App.js
+++ src/App.js
@@ -9,14 +9,26 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      msgLength: 0,
       addr: "",
+      net: "TestNet",
+      msgLength: 0,
     };
   }
 
   switchConnector = (e) => {
     Pipeline.pipeConnector = e.target.value;
     console.log(e.target.value);
+  };
+
+  switchNet = (e) => {
+    if (this.state.net === "TestNet") {
+      Pipeline.main = true;
+      this.setState({ net: "MainNet" });
+    } else {
+      Pipeline.main = false;
+      this.setState({ net: "TestNet" });
+    }
+    console.log(this.state.net);
   };
 
   handleConnect = () => {
@@ -32,14 +44,29 @@ class App extends Component {
         <div className="header">
           <p className="wallet-number">WALLET: {this.state.addr}</p>
           <div className="wallet-container">
-            <select className="wallet" onChange={this.switchConnector}>
-              <option value="myAlgoWallet">MyAlgoWallet</option>
-              <option value="WalletConnect">WalletConnect</option>
-              <option value="AlgoSigner">AlgoSigner</option>
-            </select>
-            <button className="button-primary" onClick={this.handleConnect}>
-              {this.state.addr < 5 ? "Connect Wallet" : "Connected!"}
-            </button>
+            <div className="switch-container">
+              MainNet
+              <label className="switch">
+                <input
+                  type="checkbox"
+                  onChange={(e) => {
+                    this.switchNet();
+                  }}
+                />
+                <span className="slider round"></span>
+              </label>
+              TestNet
+            </div>
+            <div className="">
+              <select className="wallet" onChange={this.switchConnector}>
+                <option value="myAlgoWallet">MyAlgoWallet</option>
+                <option value="WalletConnect">WalletConnect</option>
+                <option value="AlgoSigner">AlgoSigner</option>
+              </select>
+              <button className="button-primary" onClick={this.handleConnect}>
+                {this.state.addr < 5 ? "Connect Wallet" : "Connected!"}
+              </button>
+            </div>
           </div>
         </div>
         <div className="content">
